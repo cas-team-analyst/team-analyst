28ca9b90a889e68d6757e0c4a14ba54b18ea0725
Bryce Chamberlain <superchordate@gmail.com>
2026-04-29 08:25:27 -0300

    Clear sample run.

--- sample-data/sample-run/scripts/modules/config.py
+++
@@ -1,9 +0,0 @@
-# Standard project folder paths, relative to the scripts/ working directory.
-# Modify these values if your project uses non-standard folder names.
-
-BASE_DIR       = "../"
-RAW_DATA       = "../raw-data/"
-PROCESSED_DATA = "../processed-data/"
-SELECTIONS     = "../selections/"
-ULTIMATES      = "../ultimates/"
-OUTPUT         = "../output/"
